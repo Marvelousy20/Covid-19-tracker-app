da913a1739c038767156f7b410f198e8aaf81e7f
Afolabi Marvelous <afosmarvel@gmail.com>
2020-06-16 12:17:16 -0700

    build added

---
+++ build/precache-manifest.eb92092e64f354f5a3570e00ebae0e04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24b4a8168475ea8e45baa71a3d1821e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61ffc04a49551b962e04",
+    "url": "/static/css/main.8753fdee.chunk.css"
+  },
+  {
+    "revision": "04d11c322cfa3dbaaa54",
+    "url": "/static/js/2.2cd5cbb2.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/static/js/2.2cd5cbb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61ffc04a49551b962e04",
+    "url": "/static/js/main.ff80cd64.chunk.js"
+  },
+  {
+    "revision": "2c586889c253636bcb6f",
+    "url": "/static/js/runtime-main.e85d4211.js"
+  },
+  {
+    "revision": "b9bc190b3254c84b9910945bf6ecab74",
+    "url": "/static/media/image.b9bc190b.png"
+  }
+]);
